cd43976e3136455280e0107996e86135d14e3ecc
Abhishek Kumar Rauniyar <abhishekrauniyar004@gmail.com>
2026-03-19 21:11:34 +0530

    My portfolio

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+const up = document.getElementById("up");
+
+window.addEventListener("scroll", () => {
+    const scrollTop = window.scrollY;
+    const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const scrollPercent = (scrollTop / docHeight) * 100;
+
+    if (scrollPercent > 25) {
+        up.style.display = "block";
+    } else {
+        up.style.display = "none";
+    }
+});
+
+// up.addEventListener("click", () => {
+//     window.scrollTo({
+//         top: 0,
+//         behavior: "smooth"
+//     });
+// });
+
